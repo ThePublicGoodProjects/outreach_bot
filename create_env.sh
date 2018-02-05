caee93158f5f70321ca3f40d2d650ecc3c8ff20a
gboeing <gboeing@berkeley.edu>
2018-02-04 16:51:51 -0800

    setup files

---
+++ create_env.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ThePublicGoodProjects/outreach_bot.git
+cd outreach_bot
+conda create --name TWITTER python=3 --file requirements.txt
+source activate TWITTER
+conda info --all
+conda list
--- install_conda.sh
+++ install_conda.sh
@@ -6,6 +6,4 @@ conda config --set always_yes yes --set show_channel_urls true
 conda update conda
 conda update --all
 conda config --add channels conda-forge --force
-conda create --name TWITTER python=3 --file requirements.txt
-source activate TWITTER
 conda info --all
